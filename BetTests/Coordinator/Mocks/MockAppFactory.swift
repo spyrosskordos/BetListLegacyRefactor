7033a2a1207fea5685afe047de8c95bc0b3e7793
Spyridon Skordos <spyrosskordos@gmail.com>
2024-11-21 00:06:46 +0200

    factory coordinator tests

--- BetTests/Coordinator/Mocks/MockAppFactory.swift
+++ BetTests/Coordinator/Mocks/MockAppFactory.swift
@@ -1,25 +1,38 @@
+//
+//  MockAppFactory.swift
+//  Bets
+//
+//  Created by Spyridon Skordos on 20/11/24.
+//
+
+@testable import Bets
+@testable import BetsCore
+import UIKit
+
 final class MockAppFactory: AppFactory {
     var didCallMakeBetRepository = false
     var didCallMakeBetListCoordinator = false
 
     func makeBetRepository() -> BetRepository {
         didCallMakeBetRepository = true
-        return MockBetRepository()
+        return MockBetRepository(response: .mockResponse([]))
     }
     
     func makeBetlistCoordinator(navigation: UINavigationController, betRepository: BetRepository) -> Coordinator {
         didCallMakeBetListCoordinator = true
-        return MockBetListCoordinator()
+        return MockBetListCoordinator(navigation: navigation)
     }
 }
 
-final class MockBetRepository: BetRepository {
-    // Implement required methods or leave empty if not needed
-}
+
 
 final class MockBetListCoordinator: Coordinator {
+    var navigation: UINavigationController
     var didStart = false
     
+    init(navigation: UINavigationController) {
+        self.navigation = navigation
+    }
     func start() {
         didStart = true
     }
